4a17b7f27164663dc84f6c110f9578c6bb158990
Tansu Dasli <tansudasli@gmail.com>
2019-04-14 12:17:08 +0300

    File read completed (step 1)

--- wordcount/src/main/java/fileReaderSpout.java
+++ wordcount/src/main/java/fileReaderSpout.java
@@ -5,21 +5,47 @@ import org.apache.storm.topology.base.BaseRichSpout;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Values;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
 
-public class Spout extends BaseRichSpout {
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class fileReaderSpout extends BaseRichSpout {
 
     private SpoutOutputCollector collector;
+    private String filePath;
+    private List<String> lines;
 
     @Override
     public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
         this.collector = collector;
+
+        filePath = conf.get("filePath").toString();
+
+        //Step -1 : read file as lines
+        try {
+           lines = Files.readAllLines(Paths.get(filePath), UTF_8);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //use Files and Path ! that's the old style
+        //BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
 
     }
 
     @Override
     public void nextTuple() {
 
+        //Step -2 : read file as lines
         //toDo: emit message, use new Values() w/ comma separated list of strings
         this.collector.emit(new Values());
 
